5a2d495d6921622f451051c76acacff9979eb4f3
GV79 <glam01@uoguelph.ca>
2019-08-26 02:02:13 -0400

    services folder for future scaling and dry code

---
+++ src/services/services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Icon, notification } from 'antd'
+
+// File for using commonly used functions - simply import the one desired
+
+export const openNotification = (status, messageOne, messageTwo) => {
+  notification.open({
+    message: status ? 'Success' : 'Error',
+    duration: 2,
+    placement: 'bottomRight',
+    description: status ? messageOne : messageTwo,
+    icon: <Icon type={status ? 'smile' : 'warning'} style={{ color: status !== null ? '#108ee9' : 'red' }} />,
+  })
+}
