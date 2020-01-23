1a0c901952ea05fcda6f749cc1392765c9415c2f
GV79 <glam01@uoguelph.ca>
2020-01-23 08:40:40 -0500

    placeholders for structure

--- server/app/utility/services.js
+++ server/app/utility/services.js
@@ -2,6 +2,9 @@
  * File for commonly used backend functions
  */
 
+import { SESSION_EXPIRED } from './constants';
+
+//  Maps back-end errors to error constants to return
 export const handleErrorResponse = err => {
-  // if (err === )
+  // if (err === ) return SESSION_EXPIRED;
 };
