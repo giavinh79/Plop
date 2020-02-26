fe95a31c025602b48f5ce5bfb2d15b119fede506
GV79 <glam01@uoguelph.ca>
2020-02-26 01:04:32 -0500

    testing default axios config for cookie passing

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
